2e9fba43300d00710e7ca896becccd9c9e16fc04
Tom <thomas.spencer@codurance.com>
2020-08-17 10:55:08 +0100

    Refactor play

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,8 +1,6 @@
 defmodule FizzBuzz do
 
-  def play(min, max) do
-    Enum.each(min..max, fn(x) -> play(x) end)
-  end
+  def play(min, max), do: Enum.each(min..max, &play/1)
 
   def play(num) when (rem(num, 15) == 0), do: IO.puts "fizzbuzz"
   def play(num) when (rem(num, 5) == 0), do: IO.puts "buzz"
