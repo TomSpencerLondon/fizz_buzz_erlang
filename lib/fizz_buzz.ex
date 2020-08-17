eb7473ad8b71f6318cd1456c513065b4d556f85f
Tom <thomas.spencer@codurance.com>
2020-08-17 10:55:54 +0100

    Delete lines

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,10 +1,8 @@
 defmodule FizzBuzz do
 
   def play(min, max), do: Enum.each(min..max, &play/1)
-
   def play(num) when (rem(num, 15) == 0), do: IO.puts "fizzbuzz"
   def play(num) when (rem(num, 5) == 0), do: IO.puts "buzz"
   def play(num) when (rem(num, 3) == 0), do: IO.puts "fizz"
-
   def play(num), do: IO.puts num
 end
